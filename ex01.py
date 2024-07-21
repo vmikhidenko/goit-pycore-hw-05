1434129b80ea08f304fc7bd906c6f7b2538b6561
vmikhidenko <vladimir.mikhidenko@gmail.com>
2024-07-20 20:21:45 -0500

    exe1

--- ex01.py
+++ ex01.py
@@ -1,9 +1,12 @@
+import time
 from typing import Callable
 
-def caching_fibonacci() -> Callable[[int], int]: # setting main function
-    cache = {} # creating an empty dict
+def caching_fibonacci() -> Callable[[int], int]:
+    # defining main function
+    cache = {}
 
-    def fibonacci(n: int) -> int: # closing function
+    def fibonacci(n: int) -> int:
+        # defining closing function
         if n <= 0:
             return 0
         elif n == 1:
@@ -11,14 +14,66 @@ def caching_fibonacci() -> Callable[[int], int]: # setting main function
         elif n in cache:
             return cache[n]
         else:
-            cache[n] = fibonacci(n - 1) + fibonacci (n - 2)
-        return cache[n]
-    return fibonacci
-    
-fib = caching_fibonacci() # extracting fibonacci function
-# using the function to calculate Fibonacci numbers
-print(fib(12))
-print(fib(5))  
-print(fib(10)) # with using cahing every next calculation is stored in memory (cache)
-print(fib(20))     
+            cache[n] = fibonacci(n - 1) + fibonacci(n - 2) #calculation of fibonacci number
+            return cache[n]
 
+    return fibonacci
+
+def nocaching_fibonacci() -> Callable[[int], int]:
+    def fibonacci(n: int) -> int:
+        if n <= 0:
+            return 0
+        elif n == 1:
+            return 1
+        else:
+            return fibonacci(n - 1) + fibonacci(n - 2)
+
+    return fibonacci
+
+# Testing the functions
+fib_cached = caching_fibonacci()
+fib_not_cached = nocaching_fibonacci()
+
+# Testing caching version
+print("Testing caching version:")
+begin = time.time()
+print(fib_cached(10))
+end = time.time()
+print("Time taken to execute function with cache:", end - begin)
+
+begin = time.time()
+print(fib_cached(10))
+end = time.time()
+print("Time taken to execute function with cache (second run):", end - begin)
+
+begin = time.time()
+print(fib_cached(20))
+end = time.time()
+print("Time taken to execute function with cache:", end - begin)
+
+begin = time.time()
+print(fib_cached(20))
+end = time.time()
+print("Time taken to execute function with cache (second run):", end - begin)
+
+# Testing non-caching version
+print("\nTesting non-caching version:")
+begin = time.time()
+print(fib_not_cached(10))
+end = time.time()
+print("Time taken to execute function without cache:", end - begin)
+
+begin = time.time()
+print(fib_not_cached(10))
+end = time.time()
+print("Time taken to execute function without cache (second run):", end - begin)
+
+begin = time.time()
+print(fib_not_cached(20))
+end = time.time()
+print("Time taken to execute function without cache:", end - begin)
+
+begin = time.time()
+print(fib_not_cached(20))
+end = time.time()
+print("Time taken to execute function without cache (second run):", end - begin)
