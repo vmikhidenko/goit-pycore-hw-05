10bcb6f6f02e4be66672bddc11c7e999d79f7e40
vmikhidenko <vladimir.mikhidenko@gmail.com>
2024-07-20 19:54:03 -0500

    Exe1

---
+++ ex01.py
@@ -0,0 +1,24 @@
+from typing import Callable
+
+def caching_fibonacci() -> Callable[[int], int]: # setting main function
+    cache = {} # creating an empty dict
+
+    def fibonacci(n: int) -> int: # closing function
+        if n <= 0:
+            return 0
+        elif n == 1:
+            return 1
+        elif n in cache:
+            return cache[n]
+        else:
+            cache[n] = fibonacci(n - 1) + fibonacci (n - 2)
+        return cache[n]
+    return fibonacci
+    
+fib = caching_fibonacci() # extracting fibonacci function
+# using the function to calculate Fibonacci numbers
+print(fib(12))
+print(fib(5))  
+print(fib(10))  
+print(fib(20))     
+
